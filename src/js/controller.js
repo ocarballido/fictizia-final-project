2e3c861aebc693ad4f5ca8cbbb802f5ecf80108e
ocarballido <ocarballido@gmail.com>
2021-05-13 14:39:39 +0200

    removing unused code

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,20 +10,6 @@ class Controller {
 
 const app = new Controller(model, view)
 
-// Test adding user
-app.model.addUser('Oscar');
-app.model.addUser('Pepe');
-app.model.addUser('chini');
-app.model.addUser('bolo');
-app.model.addUser('paco');
-
-// Test adding product
-app.model.addProduct('Ron', 1, 1);
-app.model.addProduct('Sal', 0.5, 2);
-// app.model.addProduct('Sal', 0.25, 3);
-// app.model.addProduct('Sal', 0.15, 4);
-// app.model.addProduct('Sal', 0.05, 5);
-
-console.log(app.model.data);
+// console.log(app.model.data);
 
 export { app };
--- src/js/model.js
+++ src/js/model.js
@@ -1,42 +1,19 @@
-class IdGenerator {
-    constructor() {
-        this.id = IdGenerator.getID();
+const Guest = (function () {
+    let id = 0;
+    return class Guest {
+        constructor(name, expenses = 0, globalDebt = {}, realDebt = {}) {
+            this.id = ++id;
+            this.name = name;
+            this.expenses = expenses;
+            this.globalDebt = globalDebt;
+            this.realDebt = realDebt;
+        }
+        // Get initial letter
+        getInitialLetter() {
+            return this.name[0].toUppercase();
+        }
     }
-    static getID() {
-        return (Date.now() + Math.random()).toString(36);
-    }
-}
-
-class UserTwo extends IdGenerator {
-    constructor(name, expenses = 0) {
-        super();
-        this.name = name;
-        // this.money = money
-        this.expenses = expenses;
-        // this.status = status
-        this.globalDebt = {};
-        this.realDebt = {};
-    }
-    // Get initial letter
-    getInitialLetter() {
-        return this.name[0].toUppercase();
-    }
-}
-
-class User {
-    constructor(name, expenses = 0) {
-        this.name = name;
-        // this.money = money
-        this.expenses = expenses;
-        // this.status = status
-        this.globalDebt = {};
-        this.realDebt = {};
-    }
-    // Get initial letter
-    getInitialLetter() {
-        return this.name[0].toUppercase();
-    }
-}
+})();
 
 class Product {
     constructor(productTitle, productEuro, productBuyer) {
@@ -68,168 +45,15 @@ class Model {
 
     // Add new user
     addUser(userName) {
-        const user = new User(userName.toLowerCase());
+        const user = new Guest(userName.toLowerCase());
         user.id = ++this.data.lastUserId;
         this.data.usersList.push(user);
     }
 
     // Delete user from list
     deleteUser() {}
-
-    // Add new product
-    addProduct(productTitle, productEuro, productBuyer) {
-        const product = new Product(productTitle, productEuro, productBuyer);
-        this.data.productsList.push(product);
-        this.addUsersExpenses(product);
-        // this.addUsersGlobalDebt(product);
-        // this.addUsersReallDebt(product)
-        this.globalDebt(product);
-    }
-
-    // Add each user expenses
-    addUsersExpenses(product) {
-        const userMatch = this.data.usersList.findIndex((user) => {
-            return user.id === product.productBuyer;
-        });
-        this.data.usersList[userMatch].expenses += product.productPrizeCents;
-    }
-
-    globalDebt(product) {
-        const id = product.productBuyer;
-        const productBuyerIndex = this.data.usersList.findIndex((user) => {
-            return user.id === id;
-        });
-        const productBuyer = this.data.usersList[productBuyerIndex];
-        // const debtFromBuyer = this.data.usersList[productBuyerIndex]
-        this.data.usersList.forEach((user, index) => {
-            const currentUserID = user.id;
-            const buyerDebtCurrentUser = productBuyer
-            const temp = buyerDebtCurrentUser.globalDebt[currentUserID] || 0;
-            if (user.id !== id) {                
-                user.globalDebt = {
-                    ...user.globalDebt,
-                    // [id]: buyerDebtCurrentUser.globalDebt[currentUserID] === undefined ? product.productPrizeCents / this.data.usersList.length : (product.productPrizeCents / this.data.usersList.length) - buyerDebtCurrentUser.globalDebt[currentUserID],
-                    [id]: (product.productPrizeCents / this.data.usersList.length) - temp
-                }
-                
-                // console.log(currentUserID, buyerDebtCurrentUser, buyerDebtCurrentUser.globalDebt[currentUserID]);
-                console.log(buyerDebtCurrentUser.globalDebt[currentUserID], temp);
-            }
-        });
-        // console.log(productBuyer.globalDebt);
-    }
-
-    // Add each user global debt
-    addUsersGlobalDebt(product) {
-        this.data.usersList.forEach(user => {
-            if (user.id !== product.productBuyer) {
-                if (!user.globalDebt[product.productBuyer]) {
-                    user.globalDebt = {
-                        ...user.globalDebt,
-                        [product.productBuyer]: product.productPrizeCents / this.data.usersList.length
-                    }
-                } else {
-                    let debtValue = user.globalDebt[product.productBuyer]
-                    user.globalDebt = {
-                        ...user.globalDebt,
-                        [product.productBuyer]: debtValue + product.productPrizeCents / this.data.usersList.length
-                    }
-                }
-
-            }
-        });
-    }
-
-    // Add each user real debt
-    // addUsersReallDebt(product) {
-    //     this.data.usersList.reduce((acc, actualUser) => {
-    //         if (actualUser.globalDebt.hasOwnProperty(acc.name)) {
-    //             console.log('yes')
-    //         }
-    //     });
-    // }
-    addUsersReallDebt(product) {
-        // Get buyer ID, this way we can know the buyr
-        const productBuyerID = product.productBuyer;
-        // Find buyer index
-        const productBuyer = this.data.usersList.findIndex((user) => {
-            return user.id === productBuyerID;
-        });
-        // Get debt portion per user
-        const debtPortionPerUser = product.productPrizeCents / this.data.usersList.length;
-        this.data.usersList.forEach((user, index) => {
-
-            const currentUser = user.id;
-            const debtToBuyer = user.globalDebt[productBuyerID]
-            const productBuyerIndex = this.data.usersList.findIndex((user) => {
-                return user.id === productBuyerID;
-            });
-            const debtFromBuyer = this.data.usersList[productBuyerIndex]
-
-            let debtFromBuyerTwo = user.globalDebt;
-            console.log(debtFromBuyerTwo);
-
-            if (user.globalDebt.hasOwnProperty(productBuyerID)) {
-                // if (user.globalDebt[productBuyerID] < this.data.usersList[productBuyer].globalDebt[user.id]) {
-                //     user.realDebt = {
-                //         ...user.realDebt,
-                //         [productBuyerID]: 0
-                //     }
-                // } else {
-                //     user.realDebt = {
-                //         ...user.realDebt,
-                //         [productBuyerID]: user.globalDebt[productBuyerID] - this.data.usersList[productBuyer].globalDebt[user.id]
-                //     }
-                //     console.log(this.data.usersList[productBuyer].globalDebt[productBuyerID], user.globalDebt[productBuyerID])
-                // }
-
-                console.log('exist', currentUser, debtToBuyer, productBuyerIndex, debtFromBuyer);
-                // console.log(currentUser);
-            }
-        });
-    }
-
-    // Delete product from list
-    deleteProduct() {}
-
-    // Order usersList
-    orderUsersList() {
-        this.usersList.sort((a, b) => {
-            return (a.status === b.status)? 0 : b? -1 : 1;
-        });
-    }
-
-    // Calculate (This is the chicken of the chicken with rice)
-    calcDebtPerUser() {
-        const users = this.data.usersList;
-        this.data.productsList.forEach(product => {
-            let productBuyer = product.productBuyer.toLowerCase();
-            let productPize = product.productPrizeCents;
-            // console.log(product);
-            for (const user of users) {
-                if (!user.name.toLowerCase() === productBuyer) {
-                    // user.expenses += productPize;
-                    // console.log(user, user.name, productBuyer, user.expenses);
-                    // console.log(productBuyer, user);
-                    user.globalDebt = {
-                        ...user.globalDebt,
-                        [product.productBuyer]: product.productPrizeCents / users.length
-                    }
-                }
-            }
-        });
-    }
-
-    // Update data in localStorage
-    updateStorage() {}
-
-    // Toggle user status (positive/negative)
-    toggleStatus() {}
-
-    // Restart app
-    restart() {}
 };
 
 const model = new Model();
 
-export { model, UserTwo };
+export { model };
